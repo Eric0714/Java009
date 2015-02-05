7cb4180c72c854ab45a4501d768c022f5a94716a
cweng <wengchunyue2005@163.com>
2015-02-05 23:51:06 +0800

    Java009 code

--- src/org/waspec/Main.java
+++ src/org/waspec/Main.java
@@ -3,6 +3,19 @@ package org.waspec;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("This is for test only.");
+        int score = 90;
+        if (score <= 100 && score >= 90) {
+            System.out.println("A");
+        } else if (score < 90 && score >= 80) {
+            System.out.println("B");
+        } else if (score < 80 && score >= 70) {
+            System.out.println("C");
+        } else if (score < 70 && score >= 60) {
+            System.out.println("D");
+        } else if (score < 60 && score >= 0) {
+            System.out.println("E");
+        } else {
+            System.out.println("Error");
+        }
     }
 }
